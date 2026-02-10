cc6d23567017d126398b54af6fe81050b3b2165d
Faheem <faheempktl@gmail.com>
2026-02-10 11:15:18 +0530

    COnfiguration files implemented

---
+++ backend/src/test/java/com/collab/backend/CollaborativeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collab.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
